cf2bbafb9593d0161cb720567914d6cb17b6656c
Raymond Choo Juan Yong <volvagia356@gmail.com>
2014-07-23 03:57:19 +0800

    Automated Windows EXE build process

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
 import py2exe
 
+# Replace linux import with windows
+orig = open("mmu_autowifi.py",'rb').read()
+edited = orig.replace("from linux import", "from windows import")
+open("mmu_autowifi.py",'wb').write(edited)
 
 setup(windows=['gui.py'],
     options={"py2exe": {
     'bundle_files': 3,
     'compressed': True}},
     zipfile = None,
-    dlls_in_exedir = ["tcl85.dll", "tk85.dll"], data_files=["cabundle"])
+    data_files=["cabundle"])
+
+open("mmu_autowifi.py",'wb').write(orig)
